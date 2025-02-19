4649de6e5809c3da41b9bfbffb4515b64ecb9f26
pranavkolhe-apptware <pranav.kolhe@apptware.com>
2025-02-19 16:47:58 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ function Home() {
 
     const fetchProducts = async () => {
         try {
-            const url = "http://localhost:8080/products";
+            const url = "https://hrms-backend-0d8h.onrender.com/products";
             const headers = {
                 headers: {
                     'Authorization': localStorage.getItem('token')
