e295d2a89c5978d18dd3f3daf0460375502eee3d
pranavkolhe-apptware <pranav.kolhe@apptware.com>
2025-02-19 16:41:10 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+        <h1>Welcome to the Home Page</h1>
+        <p>This is the home of our MERN stack application.</p>
+    </div>
+);
+
+export default Home;
